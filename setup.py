6c67b34bba783017440cce3fbcab716075417b4c
martynipratt <martyn.pratt@digital.cabinet-office.gov.uk>
2024-02-14 16:23:50 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='computer',
+      version='1.1',
+      description='simple AWS Bedrock cli interface',
+      author='Martyn Pratt',
+      author_email='martyn.pratt@version1.com',
+      scripts=['scripts/computer'],
+     )
